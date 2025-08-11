8bf299158e2ce73ff69c2e0fb13933b907e97f21
Diaa Qassem <qassemdiaa48@gmail.com>
2025-08-11 07:56:25 +0300

    fp

--- src/org/iti/dockerBuild.groovy
+++ src/org/iti/dockerBuild.groovy
@@ -3,4 +3,5 @@ package org.iti;
 def dockerBuild(String imageName, String buildNumber) {
         sh "docker build -t ${imageName}:v${buildNumber} ."
         sh "docker images"
+        sh "docker push ${IMAGE_NAME}:${IMAGE_TAG}"
 }
--- src/org/iti/dockerPush.groovy
+++
@@ -1,6 +0,0 @@
-def call(String imageName, String buildNumber, String dockerUser, String dockerPass) {
-    stage("Docker Push") {
-        sh "echo ${dockerPass} | docker login -u ${dockerUser} --password-stdin"
-        sh "docker push ${imageName}:v${buildNumber}"
-    }
-}
